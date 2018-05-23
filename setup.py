80f23724cdf68d5f9de85736131afa63880699d8
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-23 13:42:09 -0700

    made the project installable with a setup and manifest.in file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""The setup file that makes this project installable."""
+from setuptools import find_packages, setup
+
+setup(
+    name='flaskr',
+    version='1.0.2',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
