25f7e3ea8537c304d7b232185f8989c84a992cbf
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-23 13:34:09 -0700

    created the delete route. there is no template for delete, so it only accepts a post method.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -93,3 +93,14 @@ def update(id):
             return redirect(url_for('blog.index'))
 
     return render_template('blog/update.html', post=post)
+
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    """The delete route."""
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
