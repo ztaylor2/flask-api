402e09a2a4de78d8fb7b9394ecd5e024cd67e315
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-23 12:34:42 -0700

    added the logged in user method that loads the logged in user for other views if the user is logged in.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -67,3 +67,16 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    """Load the logged in user if user is logged in."""
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
