e4631e44382d5e74315e65709abaacd2f86b91fe
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-23 13:22:38 -0700

    added a method to retrieve a post from the database based on the author id.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -46,3 +46,21 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
+
+
+def get_post(id, check_author=True):
+    """Get a post from the database based on the author id."""
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, "Post id {0} doesn't exist.".format(id))
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
