d2760b299d9af389a956b1855a1b9873e74e792a
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-23 13:23:52 -0700

    added the update view/route.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -64,3 +64,32 @@ def get_post(id, check_author=True):
         abort(403)
 
     return post
+
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    """The update view."""
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
