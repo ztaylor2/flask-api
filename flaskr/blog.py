6663596e5e8940d1ca66654c2fb4e80f30130e3d
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-23 13:04:25 -0700

    added the index route, which uses a join query to grab all users and blog posts and returns them to the view.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -7,3 +7,15 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
+
+
+@bp.route('/')
+def index():
+    """The index for the blog, displays all blog posts."""
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
