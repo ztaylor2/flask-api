a6c2de62a6c9ce388cc6652b8b692efd03cf270d
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-23 12:31:22 -0700

    created the login view.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -10,8 +10,10 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
+    """The register view."""
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -38,3 +40,30 @@ def register():
         flash(error)
 
     return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    """The login view."""
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
